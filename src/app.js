83b9f238d4dd1fe8ec4d5e06234e7e05bf9543de
Shiv Buddha <shivakanthb@gmail.com>
2019-03-05 19:51:16 +0530

    async call with create issue  successful
    async call with create issue  successful

--- src/app.js
+++ src/app.js
@@ -11,9 +11,10 @@ const { JovoDebugger } = require('jovo-plugin-debugger');
 const { FileDb } = require('jovo-db-filedb');
 
 const sn=require('servicenow-rest-api'); // service now connect
-const Promise = require('request-promise-native');
+const rp = require('request-promise-native');
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('./info.properties');
+const axios= require('axios');
 
 
 
@@ -50,16 +51,40 @@ app.setHandler({
     },
 
     async NewTaskIntent() {
-
-        
-       
-        taskNumber = await createNewTaskInServiceNow(this.$inputs);
-
-        if(taskNumber != "0") {
+               
+      
+    var ret =null
+    const data={
+        'short_description':this.$inputs.taskName.value,
+        'urgency':this.$inputs.urgency.value
+    };
+    
+    const options={
+        url:`https://${properties.get('_INSTANCE')}.service-now.com/api/now/table/incident?sysparm_input_display_value=true&sysparm_display_value=true`,
+        method:'post',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        data:data,
+        auth:{
+            username:properties.get('_USERID'),
+            password:properties.get('_PASSWORD')
+        }
+    }
+   await axios(options).then((val)=>{
+        taskNumber = val.data.result.number
+        if(taskNumber != null) {
             this.ask('Created task susccessfully with number  ' + taskNumber ,'What next');  
         } else {
             this.ask('Task could not be created , Please try again '  ,'What next');  
         }
+        console.log(val.data.result);
+    }).catch((err)=>{
+        console.log(err);       
+    });
+
+
         
     },
 
@@ -71,22 +96,11 @@ app.setHandler({
 
 });
 
-async function createNewTaskInServiceNow(inputs) {
-    
-    ServiceNow.Authenticate();
-        
-    const data={
-        'short_description':inputs.taskName.value,
-        'urgency':inputs.urgency.value
-    };
 
-    ServiceNow.createNewTask(data,'incident',res=>{
-        console.log(res);
-        taskNumber = res.number;
-        console.log("taskNumber =" + taskNumber);
-    });
+async function createNewTaskInServiceNow(inputs,alexa) {
 
-    return taskNumber;
+
+ return ret
 }
 
 module.exports.app = app;
